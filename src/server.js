b1b1e1b0c4e4f46869686c7c9ee73471386ca1bc
seongchan <xoom3299@gmail.com>
2021-06-29 17:56:21 +0900

    #4.2 Cleaning the Code

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express"; //express로부터 express를 불러옴
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import usersRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+const PORT = 4000;
+
+const app = express(); //express함수로 익스프레스 앱을 만듬
+
+const logger = morgan("combined");
+
+app.use("/", globalRouter);
+app.use("/users", usersRouter);
+app.use("/videos", videoRouter);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`); //listen의 콜백함수 구현
+
+app.listen(PORT, handleListening); // 서버는 사용자가 보내는 request를 항상 listen하고 있어야한다.(상호작용하기위해)

78bd0b979877a718698b906d2c99ebacdfc086d1
seongchan <xoom3299@gmail.com>
2021-06-29 19:57:54 +0900

    #4.6  Planning Routes

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { remove, edit } from "../controllers/userController";
+import { remove, edit, logout, see } from "../controllers/userController";
 
 
 
 const usersRouter = express.Router();
 
-
+usersRouter.get("/logout", logout);
 usersRouter.get("/edit", edit);
 usersRouter.get("/delete", remove);
+usersRouter.get(":id", see);
 
 export default usersRouter;

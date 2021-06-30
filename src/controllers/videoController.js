e531b106f4ee131adf5d1d9e6f7e86132613d857
seongchan <xoom3299@gmail.com>
2021-06-30 17:21:37 +0900

    #5.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 //global Router
 export const trending = (req, res) => {
-    res.render("home");
-};
+    res.render("home", {pageTitle : "Home", potato : "I love Potato"}); //render은 2가지 인수를 받는데, 첫번째는 view의 이름이고 두번째는 템플릿에 보낼 변수다. 변수는 원하는 만큼 보낼 수 있다.
+} 
 export const search = (req, res) => res.send("Search Video");
 
 //video Router

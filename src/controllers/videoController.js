80a3bc5d3f0fd879487af25c62a22a11b162f125
seongchan <xoom3299@gmail.com>
2021-07-01 15:50:15 +0900

    #6.2 Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -38,9 +38,16 @@ export const watch = (req, res) => {
     const video = videos[id-1];
     return res.render("watch", {pageTitle : `watch ${video.title}`, video});
 };
-export const edit = (req, res) => {
-    res.render("edit");
+export const getEdit = (req, res) => {
+    const {id} = req.params;
+    const video = videos[id-1];
+    res.render("edit", {pageTitle : `Editing ${video.title}`, video});
 };
+
+export const postEdit = (req, res) => {
+    //pass
+}
+
 export const deleteVideo = (req, res) =>{
     console.log(req.params);
     return res.send("Delete Video")

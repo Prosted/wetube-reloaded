329fa92c984a994bc185092271ba2678fcb105fa
seongchan <xoom3299@gmail.com>
2021-07-03 17:13:58 +0900

    #6.18 More Schema

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title : {type : String, required : true},
-    description : {type : String, required : true},
+    title : {type : String, required : true, trim : true, uppercase : true, minLength : 5, maxLength : 20},
+    description : {type : String, required : true, minLength : 20, maxLength : 140},
     createdAt : {type:Date, required : true, default : Date.now},
     hashtags:[{type : String}], //문자열 배열 타입
     meta : {

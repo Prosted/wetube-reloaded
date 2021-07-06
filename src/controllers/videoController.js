ffb9bf4d05748fa7623b243e4444febd21a60bd0
seongchan <xoom3299@gmail.com>
2021-07-06 17:13:41 +0900

    #6.27 Search part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,22 +2,17 @@ import Video from "../models/Video";
 
 
 //global Router
-/* 비동기 처리방식 콜백함수
-export const home = (req, res) => {
-    Video.find({},  (error, videos) => {
-        //에러가 발생했는지 여부를 조건문으로 확인
-        if(error)
-            return res.render("server-error");
-        else
-           return res.render("home", {pageTitle : "Home", videos}); //render은 2가지 인수를 받는데, 첫번째는 view의 이름이고 두번째는 템플릿에 보낼 오브젝트다. 오브젝트 안 변수는 원하는 만큼 보낼 수 있다.
-    });
-} 
-*/
-
-//동기처리방식 async & await
 export const home = async(req, res) => { 
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({createdAt : "desc"});
     return res.render("home", {pageTitle : "Home", videos});
+}
+
+export const search = (req, res) =>{
+    const {keyword} = req.query;
+    console.log(keyword);
+    if(keyword){
+    }
+    return res.render("search", {pageTitle : "Search"});
 }
 
 //video Router

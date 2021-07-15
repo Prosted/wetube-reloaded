c06741aef8b14f1340dd6d3a19aa0ea1010ffd42
seongchan <xoom3299@gmail.com>
2021-07-15 15:38:11 +0900

    #7.1 Create Account part Two

--- src/db.js
+++ src/db.js
@@ -4,9 +4,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useFindAndModify : false,
+    useCreateIndex : true,
 });
-
-mongoose.set('useCreateIndex', true)
 
 const db = mongoose.connection;
 

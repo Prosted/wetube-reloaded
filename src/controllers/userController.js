49c287ff482c66a53867107847e6c4dc96974221
seongchan <xoom3299@gmail.com>
2021-07-20 22:15:21 +0900

    #7.18 Github Login part Three

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -57,7 +57,7 @@ export const see = (req, res) => res.send("see User's Profile");
 export const startGithubLogin = (req, res) => {
     const baseUrl = "https://github.com/login/oauth/authorize";
     const config = {
-        client_id : "e3cc56e9a1172bedfcaa",
+        client_id : process.env.GH_CLIENT,
         allow_signup : false,
         scope : "read:user user:email",
     }
@@ -66,7 +66,21 @@ export const startGithubLogin = (req, res) => {
     return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {
-    console.log("Github Login success!");
-    res.redirect("/");
+export const finishGithubLogin = async(req, res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token";
+    const config = {
+        client_id : process.env.GH_CLIENT,
+        client_secret : process.env.GH_SECRET,
+        code : req.query.code,
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const data = await fetch(finalUrl, {
+        method:"POST",
+        headers:{
+            Accept : "application/json",
+        },
+    });
+    const json = await data.json();
+    console.log(json);
 }

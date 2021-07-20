3976a7e33c8f986d892902c2ef39cbadae431f0f
seongchan <xoom3299@gmail.com>
2021-07-20 15:43:50 +0900

    #7.12 MongoStore

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 
 import express from "express";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import morgan from "morgan";
 import { localsMiddleware } from "./middlewares";
 import rootRouter from "./routers/rootRouter";
@@ -22,6 +23,7 @@ app.use(session({
     secret:"Hello!",
     resave : true,
     saveUninitialized : true,
+    store: MongoStore.create({mongoUrl : "mongodb://127.0.0.1:27017/wetube"}),
 }));
 
 // app.use((req, res, next)=>{

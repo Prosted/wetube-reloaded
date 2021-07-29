c76cd21e98e74d4cad485e19d5fd30bcc75c56eb
seongchan <xoom3299@gmail.com>
2021-07-29 19:29:44 +0900

    #8.8 Static Files and Recap

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ app.use(session({
 }));
 
 app.use(localsMiddleware);
-
+app.use("/uploads", express.static("uploads"));
 app.use("/", rootRouter);
 app.use("/users", usersRouter);
 app.use("/videos", videoRouter);

c531e5f2fbf9bb8ad003409af1c476cd14a0d672
seongchan <xoom3299@gmail.com>
2021-08-03 17:50:35 +0900

    #9.4 SCSS Loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,29 @@ module.exports = {
         path : path.resolve(__dirname, "assets", "js"),
     },
     module : {
-        rules : [{
-            test : /\.js$/,
-            use : {
-                loader : "babel-loader",
-                options : {
-                    presets: [
-                        ['@babel/preset-env', { targets: "defaults" }]
-                    ],
+        rules : [
+            {
+                test : /\.js$/,
+                use : {
+                    loader : "babel-loader",
+                    options : {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ],
+                    }
                 }
+            },
+            {
+                test : /\.scss$/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                  ],
             }
-        }],
+        ],
     }
 };

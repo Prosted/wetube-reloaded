ea22ccdbdecfe20dc6e68c5ae3b893e29f560643
seongchan <xoom3299@gmail.com>
2021-08-05 18:57:54 +0900

    #11.1 Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,24 @@
-console.log("watch");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+
+const handlePlayBtn = (event) => {
+    if(video.paused){
+        video.play();
+    }
+    else{
+        video.pause();
+    }
+}
+
+const handlePause = (event) => playBtn.innerHTML = "Pause";
+const handlePlay = (event) => playBtn.innerHTML = "Play";
+
+playBtn.addEventListener("click", handlePlayBtn);
+video.addEventListener("play", handlePlay);
+video.addEventListener("pause", handlePause);
+muteBtn.addEventListener("click", handleMute);
+

e638d9a203c76a91cb66a75c0e25a4fedf654e43
seongchan <xoom3299@gmail.com>
2021-08-05 21:17:34 +0900

    #10.4 Duration and Current Time

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,12 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
+
+console.log(currenTime);
+console.log(totalTime);
 
 let volumeValue =0.5;
 video.volume = volumeValue;
@@ -14,20 +18,20 @@ const handlePlayBtn = (event) => {
     else{
         video.pause();
     }
-    playBtn.innerHTML = video.paused ? "Play" : "Pause";
+    playBtn.innerText = video.paused ? "Play" : "Pause";
 }
 
 const handleMute = (event) => {
     if(video.muted){
         video.muted = false;
-        if(Number(video.volume) === 0 ){
+        if(video.volume === 0 ){
             video.volume = 0.5;
             volumeValue = 0.5;
         }
     }else{
         video.muted = true;
     }
-    muteBtn.innerHTML = video.muted ? "UnMute" : "Mute";
+    muteBtn.innerText = video.muted ? "UnMute" : "Mute";
     volume.value = video.muted ? 0 : volumeValue;
 }
 
@@ -43,10 +47,24 @@ const handleVolume = (event) => {
     if(video.volume === 0){
         video.muted = "true";
     }
-    muteBtn.innerHTML = video.muted ? "UnMute" : "Mute";
+    muteBtn.innerText = video.muted ? "UnMute" : "Mute";
 }
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = Math.floor(video.duration);
+  };
+  
+  const handleTimeUpdate = () => {
+    currenTime.innerText = Math.floor(video.currentTime);
+  };
 
 playBtn.addEventListener("click", handlePlayBtn);
 muteBtn.addEventListener("click", handleMute);
 volume.addEventListener("input", handleVolume);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+
+if (video.readyState == 4) {
+    handleLoadedMetadata();
+}
 
